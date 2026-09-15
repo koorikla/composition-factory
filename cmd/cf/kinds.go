ec617a5a68fcf47f4f90308f55049f11626362cb
Kaur Kallas <kaur.kallas@velocitaconsultancy.uk>
2026-09-15 04:49:09 +0300

    fix(cli): defer cached providers on blank blueprint (CF-485, #397)

--- cmd/cf/kinds.go
+++ cmd/cf/kinds.go
@@ -36,6 +36,9 @@ func (c *KindsCmd) Run(out io.Writer) error {
 	}
 
 	refs := AssembleProviders(store, b, nil, false)
+	if len(refs) == 0 {
+		refs, _ = store.List()
+	}
 
 	idx, err := api.BuildIndex(store, refs, b, blueprintDir)
 	if err != nil {
